0aeb52c8f8f1652fc00b85bcbe1ed0f46cd0b92b
roberto.alsina <roberto.alsina@2e26d4c3-2b28-0410-a8b2-65720667fe1a>
2007-01-29 15:32:29 +0000

    Beginning of the test suite, fix rcptcheckscdb

--- tests/test_cdb.c
+++ tests/test_cdb.c
@@ -1,8 +1,24 @@
 #include <check.h>
 
-START_TEST (test_name)
+#include <cdb.h>
+#include "../cdbutils.h"
+
+START_TEST (test_cdb)
 {
     /* unit test code */
+    bstring fname=bfromcstr("test_data.cdb");
+    bstring addr=bfromcstr("JOE@whatever.org");
+
+    fail_unless ( lineincdb (addr,fname) == 1 );
+
+    addr=bfromcstr("jane@whatever.org");
+
+    fail_unless ( lineincdb (addr,fname) == 0 );
+
+    fname=bfromcstr("notexisting");
+
+    fail_unless ( lineincdb (addr,fname) == -1 );
+
 }
 END_TEST
 
