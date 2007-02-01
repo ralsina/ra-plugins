7a8eab2cfca8278f529ca7321c6eced358eaabca
roberto.alsina <roberto.alsina@2e26d4c3-2b28-0410-a8b2-65720667fe1a>
2007-02-01 14:01:31 +0000

    Oops, missing file!

---
+++ cdbutils.c
@@ -0,0 +1,31 @@
+#include <cdb.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "cdbutils.h"
+
+int
+lineincdb (bstring data, bstring fname)
+{
+  int retval = 1;
+  cdbi_t vlen;
+  int fd;
+  fd = open (fname->data, O_RDONLY);    /* open a CDB file */
+  if (fd == -1)
+    {
+      retval = -1;
+    }
+
+  else
+    {
+      btolower (data);
+      if (cdb_seek (fd, data->data, data->slen, &vlen) <= 0)
+        {
+          retval = 0;
+        }
+      close (fd);
+    }
+  return retval;
+}
