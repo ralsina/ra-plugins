4dd97c0f9391ecab380398dc707074d926188e63
roberto.alsina <roberto.alsina@2e26d4c3-2b28-0410-a8b2-65720667fe1a>
2007-02-09 02:00:23 +0000

    Organize better the tests.

--- tests/test_dns.c
+++ tests/test_dns.c
@@ -7,26 +7,40 @@
 
 bstring testdir;
 
-START_TEST (test_mailservers)
+START_TEST (test_mailservers_a)
 {
   struct bstrList *list;
   int r = mailservers (bfromcstr ("planetkde.org"), &list);
   fail_unless (r == 1 && biseq (list->entry[0], bfromcstr ("67.18.167.98")));
+}
+END_TEST 
 
-  r = mailservers (bfromcstr ("netmanagers.com.ar"), &list);
+START_TEST (test_mailservers_mx)
+{
+  struct bstrList *list;
+  int r = mailservers (bfromcstr ("netmanagers.com.ar"), &list);
   fail_unless (r == 2
                && biseq (list->entry[0], bfromcstr ("mx1.netmanagers.com.ar"))
                && biseq (list->entry[1], bfromcstr ("mx1.lunix.com.ar")));
 
-  r = mailservers (bfromcstr ("idont.exist"), &list);
+}
+END_TEST 
+START_TEST (test_mailservers_unknown)
+{
+  struct bstrList *list;
+  int  r = mailservers (bfromcstr ("idont.exist"), &list);
   fail_unless (r == -2);
 
 }
-END_TEST Suite * dns_suite (void)
+END_TEST
+
+Suite * dns_suite (void)
 {
   Suite *s = suite_create ("DNS");
   TCase *dns_core = tcase_create ("Core");
-  tcase_add_test (dns_core, test_mailservers);
+  tcase_add_test (dns_core, test_mailservers_a);
+  tcase_add_test (dns_core, test_mailservers_mx);
+  tcase_add_test (dns_core, test_mailservers_unknown);
   suite_add_tcase (s, dns_core);
   return s;
 
